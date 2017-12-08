26f6de887ad3ef449b75b294f8646dd197e583cf
YarnSeemannsgarn <janschlenker1990@gmail.com>
2017-12-08 16:02:14 +0100

    Adds input parsing

--- kraken.php
+++ kraken.php
@@ -2,18 +2,47 @@
 
 require_once 'kraken-api-client/php/KrakenAPIClient.php';
 
+// Handle command line input
+$help = "usage: php kraken.php <command> [<options>]
+
+Commands:
+ p            Shows pairs
+";
+
+if(count($argv) < 2 || $argv[1] != 'p') {
+    echo $help;
+    exit;
+}
+
+
+// Initialization
 $ini_array = parse_ini_file('api.ini');
 $kraken = new \Payward\KrakenAPI($ini_array['API_KEY'], $ini_array['API_PRIVATE_KEY']);
 
-$request = array(
-    'pair'  => "XETHZEUR"
-);
+// Pairs
+if($argv[1] == 'p') {
+    while(true) {
+        try {
+            $res = $kraken->QueryPublic('AssetPairs');
+            break;
+        }
+        catch (Payward\KrakenAPIException $e) {
+            echo "Exception during querying... Trying again!\n";
+        }
+    }
+    
+    $pairs = array();
+    foreach (array_keys($res['result']) as $pair) {
+        if(strpos($pair, 'EUR') !== false) {
+            array_push($pairs, $pair);
+        }
+    }
+    
+    print_r($pairs);    
+}
 
-// $res = $kraken->QueryPublic('AssetPairs');
-// $res = $kraken->QueryPublic('Ticker', $request);
 
-$res = $kraken->QueryPrivate('TradeBalance', array('asset' => 'ZEUR'));
-
-print_r($res);
+//$res = $kraken->QueryPublic('Ticker', $request);
+//$res = $kraken->QueryPrivate('TradeBalance', array('asset' => 'ZEUR'));
 
 ?>
